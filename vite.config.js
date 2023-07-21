aefd3ae06f51c3b8d5a678155f19e9d2c7d01ef7
boonta-chaiplum <139052829+boonta-chaiplum@users.noreply.github.com>
2023-07-21 13:14:24 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 import handlebars from 'vite-plugin-handlebars';
 
 export default {
-  base: '/portfolio/',
+  base: '/',
   root: path.resolve(__dirname, 'src'),
   resolve: {
     alias: {
